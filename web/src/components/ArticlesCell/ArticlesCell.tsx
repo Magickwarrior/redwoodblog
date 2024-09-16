4c77b6a3e1ca8cddc098ca0213c5281938164910
Fernando Spitaliere <fernando.spitaliere@gmail.com>
2024-09-16 14:40:23 +0300

    Basic Routing

---
+++ web/src/components/ArticlesCell/ArticlesCell.tsx
@@ -0,0 +1,38 @@
+import type { ArticlesQuery } from 'types/graphql'
+
+import type {
+  CellSuccessProps,
+  CellFailureProps,
+  TypedDocumentNode,
+} from '@redwoodjs/web'
+
+import Article from 'src/components/Article'
+
+export const QUERY: TypedDocumentNode<ArticlesQuery> = gql`
+  query ArticlesQuery {
+    articles: posts {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }: CellFailureProps) => (
+  <div style={{ color: 'red' }}>Error: {error.message}</div>
+)
+
+export const Success = ({ articles }: CellSuccessProps<ArticlesQuery>) => {
+  return (
+    <>
+      {articles.map((article) => (
+        <Article key={article.id} article={article} />
+      ))}
+    </>
+  )
+}
